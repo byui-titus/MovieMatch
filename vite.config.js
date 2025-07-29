f1e7c001a428a6916942b40baa13edcbf2b22da8
byui-titus <titusrayz33@gmail.com>
2025-07-29 06:18:17 +0300

    dist

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,20 @@
-import { resolve } from "path";
-import { defineConfig } from "vite";
+import { fileURLToPath } from 'url';
+import { dirname, resolve } from 'path';
+import { defineConfig } from 'vite';
+
+const __filename = fileURLToPath(
+    import.meta.url);
+const __dirname = dirname(__filename);
 
 export default defineConfig({
-    root: "/",
-
+    root: "./",
     build: {
         outDir: "../dist",
         rollupOptions: {
             input: {
-                main: resolve(__dirname, "/index.html"),
-                watchlist: resolve(__dirname, "/watchlist.html"),
-                movieDetails: resolve(__dirname, "/movieDetails.html"),
+                main: resolve(__dirname, "index.html"),
+                watchlist: resolve(__dirname, "watchlist.html"),
+                movieDetails: resolve(__dirname, "movieDetails.html"),
             },
         },
     },
